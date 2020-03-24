3ab430c1c82a343e5c9e4979fd9279a1255710ac
yhimanen <y.himanen@gmail.com>
2020-03-24 09:04:24 +0200

    cross origin added to dictionary controller

--- src/main/java/fi/academy/final_project_back/DictionaryController.java
+++ src/main/java/fi/academy/final_project_back/DictionaryController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api")
 public class DictionaryController {
 
