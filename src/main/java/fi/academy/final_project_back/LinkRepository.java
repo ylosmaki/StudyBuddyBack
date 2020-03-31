29845b2230468b83f1d85e04d01568baa73f02f2
MaijaYlosmaki <maija.ylosmaki@gmail.com>
2020-03-31 11:23:42 +0300

    finds links by week number

--- src/main/java/fi/academy/final_project_back/LinkRepository.java
+++ src/main/java/fi/academy/final_project_back/LinkRepository.java
@@ -5,9 +5,13 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LinkRepository extends CrudRepository<Link, Integer> {
 
     @Query("SELECT link FROM Link link WHERE link.id = :id")
     Link getById(@Param("id") Integer id);
+
+    List<Link> findAllByWeek(Integer week);
 }
